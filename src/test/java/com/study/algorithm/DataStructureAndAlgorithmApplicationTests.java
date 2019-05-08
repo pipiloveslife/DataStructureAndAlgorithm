9d69adfea277eaf3dc080f4bbb7ae250ec2b146e
高垠琪 <pain_zzz@163.com>
2019-05-08 09:37:10 +0800

    git repository init

---
+++ src/test/java/com/study/algorithm/DataStructureAndAlgorithmApplicationTests.java
@@ -0,0 +1,16 @@
+package com.study.algorithm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DataStructureAndAlgorithmApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
